1b4937d68a80acfee14bf783e72849dfa05f4744
nganbui <nganbui.23112003@gmail.com>
2026-09-15 13:49:11 +0700

    chore: add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { setupSwagger } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,14 +17,7 @@ async function bootstrap() {
     }),
   );
 
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle('Mood Diary API')
-    .setDescription('Backend API for the Mood Diary learning project')
-    .setVersion('0.1.0')
-    .addBearerAuth()
-    .build();
-  const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('docs', app, document);
+  setupSwagger(app);
 
   const port = configService.get<number>('PORT', 3001);
   await app.listen(port);
